ca290e313b9db9295b388e6b0f6260607e746915
Marcelo de Moraes Serpa <boss@fullofcaffeine.com>
2015-10-05 23:37:50 -0500

    Simplified

--- app/services/csrf.js
+++ app/services/csrf.js
@@ -1,9 +1,10 @@
 import Ember from 'ember';
 import { request } from 'ic-ajax';
-import { get as readFromConfig } from './config';
 
-export default Ember.Object.extend({
+export default Ember.Service.extend({
   onAjaxComplete: function() {
+    this.fetchToken();
+
     var _this = this;
     Ember.$(document).on("ajaxComplete", function(event, xhr, settings) {
       var csrf_param = xhr.getResponseHeader('X-CSRF-Param'),
@@ -14,6 +15,7 @@ export default Ember.Object.extend({
       }
     });
   }.on('init'),
+
   setPrefilter: function() {
     var token = this.get('data').token;
     var preFilter = function(options, originalOptions, jqXHR) {
@@ -21,6 +23,7 @@ export default Ember.Object.extend({
     };
     $.ajaxPrefilter(preFilter);
   },
+
   setData: function(data) {
     var param = Ember.keys(data)[0];
     this.set('data', { param: param, token: data[param] });
@@ -28,6 +31,7 @@ export default Ember.Object.extend({
 
     return this.get('data');
   },
+
   fetchToken: function() {
     var promise;
     var _this = this;
@@ -40,7 +44,7 @@ export default Ember.Object.extend({
       if (!Ember.isEmpty(token)) {
         promise = Ember.RSVP.resolve({'authenticity_token': token });
       } else {
-        promise = request(readFromConfig('url'));
+        promise = request('/api/v1/csrf');
       }
 
       promise = promise.then(function(data) {
